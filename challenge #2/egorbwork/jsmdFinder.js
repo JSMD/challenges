e2ebf7b92497324ad01cc19a8766a112e716bcf2
Egor Boaghi <egorbwork@gmail.com>
2017-02-19 04:43:08 +0200

    Fix in challenge #2 count of occurrences for cases when input doesn't contain required word.

--- challenge #2/egorbwork/jsmdFinder.js
+++ challenge #2/egorbwork/jsmdFinder.js
@@ -1,24 +1,20 @@
-function findWordInInput(input, word = 'JSMD') {
+function findWordOccurrencesInInput(input, word = 'JSMD') {
     let stringifiedInput = JSON.stringify(input);
     let regexp = RegExp(word, 'g');
+    let matches;
 
-    if (regexp.exec(stringifiedInput)) {
-        let result = 0;
-        do {
-            result++;
-        } while (regexp.exec(stringifiedInput) !== null);
-
-        return result;
+    if (matches = stringifiedInput.match(regexp)) {
+        return matches.length;
     } else {
-        return false;
+        return 0;
     }
 }
 
-function testFindWordInInput() {
-    console.log(findWordInInput([ [ [ "JSMD" ] ] ]));
-    console.log(findWordInInput({ a: { b: { c: "JSMD" } } }));
-    console.log(findWordInInput({ a: { b: { c: 'JSMD' }, d: "JSMD", JSMD: '22', text: '\'JSMD\'"JSMD"' } }));
-    console.log(findWordInInput({ a: [ "random-string", "JSDM", "JMSD", "J S M D", "BDSM" ] }));
+function testFindWordOccurrencesInInput() {
+    console.log(findWordOccurrencesInInput([ [ [ "JSMD" ] ] ]));
+    console.log(findWordOccurrencesInInput({ a: { b: { c: "JSMD" } } }));
+    console.log(findWordOccurrencesInInput({ a: { b: { c: 'JSMD' }, d: "JSMD", JSMD: '22', text: '\'JSMD\'"JSMD"' } }));
+    console.log(findWordOccurrencesInInput({ a: [ "random-string", "JSDM", "JMSD", "J S M D", "BDSM" ] }));
 }
 
 testFindWordInInput();
