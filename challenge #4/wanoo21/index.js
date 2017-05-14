d7040fc6f9e4cfb4c150ae7db35f49391ca3ade0
Ion Prodan <ionprodan@Ions-MacBook-Pro.local>
2017-05-14 21:45:07 +0300

    Right chemical symbols :)

--- challenge #4/wanoo21/index.js
+++ challenge #4/wanoo21/index.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const symbols = require('./chemical-symbols.json')
-const word = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+const phrase = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
 const foundSymbols = []
 
 const findSymbols = (() => {
   symbols.forEach(symbol => {
-    const output = new RegExp(symbol.symbol, 'i').exec(word)
+    const output = new RegExp(symbol.symbol, 'i').exec(phrase)
     output && foundSymbols.push(symbol)
   })
   return foundSymbols
