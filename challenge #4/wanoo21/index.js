af00749bdade3194696759b3fc1d99cd5e8d8986
Ion Prodan <ionprodan33@gmail.com>
2017-05-20 08:53:56 +0300

    Correct order from phrase

--- challenge #4/wanoo21/index.js
+++ challenge #4/wanoo21/index.js
@@ -1,15 +1,18 @@
 'use strict';
 
 const symbols = require('./chemical-symbols.json')
-const phrase = process.argv[3] || 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-const foundSymbols = []
+const phrase = process.argv[3] || 'NICU'
 
 const findSymbols = (() => {
+  const foundSymbols = []
   symbols.forEach(symbol => {
     const output = new RegExp(symbol.symbol, 'i').exec(phrase)
-    output && foundSymbols.push(symbol)
+    output && foundSymbols.push(Object.assign(symbol, { index: output.index }))
   })
-  return foundSymbols
+  return foundSymbols.sort((s, s2) => s.index - s2.index).map(symbol => {
+    delete symbol.index
+    return symbol
+  })
 })()
 
 console.log(findSymbols)
