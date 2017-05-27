b95f2d62dcf68a04b12982f38ebbcf1700d9dde1
Kirill Borzov <oresh.work@gmail.com>
2017-05-27 23:14:29 +0300

    issue: some clean up and all examples.

--- challenge #1/oresh/index.js
+++ challenge #1/oresh/index.js
@@ -1,11 +1,10 @@
 function count(str) {
 	var out = {};
-	for (var i = str.length-1, st = 1; i >= 0; i-- && st++) {
+	for (var i = str.length-1, el_length = 1; i >= 0; i-- && el_length++) {
 		if (str[i] != str[i].toLowerCase()) {
-			var element = str.substr(i, st)
-			if (out[element]) out[element] += 1
-			else out[element] = 1
-			st = 0
+			var element = str.substr(i, el_length)
+			if (out[element]) out[element] += 1; else out[element] = 1
+			el_length = 0
 		}
 	}
 	return out;
@@ -28,11 +27,13 @@ function replacer(input, regExp) {
 }
 
 function parseFormula(input) {
-	input = input.replace(/\[/g,"(").replace(/\]/g,")")
+	input = input.replace(/\[/g, "(").replace(/\]/g, ")")
 	input = replacer(input, new RegExp(/\([0-9a-zA-Z]+\)[0-9]+/))
 	input = replacer(input, new RegExp(/[A-Z][0-9]+/))
 	input = replacer(input, new RegExp(/[A-Z][a-z][0-9]+/))
 	return count(input);
 }
 
-console.log(parseFormula("K4[ON(SO3)2]2Cl3Mg2"));
+console.log(parseFormula("CH3COOH"));
+console.log(parseFormula("Al2SO4(CH3CO2)4"));
+console.log(parseFormula("K4[ON(SO3)2]2"));
