8a75d0d93ff51b18a59411c41256ff54ebe511a9
Egor Boaghi <egorbwork@gmail.com>
2017-09-27 08:27:43 +0300

    egorbwork challenge 6 adjust application for cloud generator usage and add it to the cloud html.

--- challenge #6/egorbwork/application.js
+++ challenge #6/egorbwork/application.js
@@ -1,48 +1,23 @@
+let lineWidth = 5;
+let cloudGeneratorBuilder = new CloudGeneratorBuilder(10, 1000, lineWidth);
+let cloudGenerator = cloudGeneratorBuilder.getCloudGenerator();
+cloudGeneratorBuilder.setNumberOfCircles(8);
 document.addEventListener("DOMContentLoaded", function() {
+    let cloud = cloudGenerator.next().value;
     // Test data:
-    let drawingEngine = new CloudDrawingEngine(document.getElementById('cloud').getContext('2d'));
-    let a = new CloudCircle(new Point(200, 200), 70);
-    let b = new CloudCircle(new Point(100, 100), 100);
-    let c = new CloudCircle(new Point(250, 250), 100);
-    let e = new CloudCircle(new Point(300, 300), 80);
-    let f = new CloudCircle(new Point(300, 240), 90);
-
-    a.calculateCloudTangentsForCircle(b);
-    a.calculateCloudTangentsForCircle(c);
-    e.calculateCloudTangentsForCircle(c);
-    f.calculateCloudTangentsForCircle(e);
-    f.calculateCloudTangentsForCircle(c);
-    let cloud = [
-        a,
-        b,
-        c,
-        e,
-        f
-    ];
-    for (let d of a.tangents) {
-        d.setUsed(true);
-        for (let circle of cloud) {
-            if ((circle !== d.firstCircle && circle !== d.secondCircle) && circle.isPointInsideOfCircle(d.point)) {
-                d.setUsed(false);
-            }
-        }
-
-    }
-    for (let d of e.tangents) {
-        d.setUsed(true);
-        for (let circle of cloud) {
-            if ((circle !== d.firstCircle && circle !== d.secondCircle) && circle.isPointInsideOfCircle(d.point)) {
-                d.setUsed(false);
-            }
-        }
-    }
-    for (let d of f.tangents) {
-        d.setUsed(true);
-        for (let circle of cloud) {
-            if ((circle !== d.firstCircle && circle !== d.secondCircle) && circle.isPointInsideOfCircle(d.point)) {
-                d.setUsed(false);
-            }
-        }
-    }
+    // let a = new CloudCircle(new Point(200, 200), 70);
+    // let b = new CloudCircle(new Point(100, 100), 100);
+    // let c = new CloudCircle(new Point(250, 250), 100);
+    // let e = new CloudCircle(new Point(300, 300), 80);
+    // let f = new CloudCircle(new Point(300, 240), 90);
+    //
+    // let testCloud = [
+    //     a,
+    //     b,
+    //     c,
+    //     e,
+    //     f
+    // ];
+    let drawingEngine = new CloudDrawingEngine(document.getElementById('cloud').getContext('2d'), lineWidth);
     drawingEngine.drawCloud(cloud);
 });
